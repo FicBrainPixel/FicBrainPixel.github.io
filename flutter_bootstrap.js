602ab5f1d75ee3531d161d57fd639ccadca90fb4
piyush <ficbrainpixel@gmail.com>
2025-02-23 04:08:45 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "809567383"
+    serviceWorkerVersion: "875012085"
   }
 });
